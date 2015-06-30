9f8bad062b75126aee1fdd1af17afdc75b8736e9
Bill Brown <b@illbrown.com>
2015-06-30 17:47:27 -0500

    bill sprite

--- js/scripts.js
+++ js/scripts.js
@@ -51,6 +51,7 @@ jQuery( document ).ready( function( $ ) {
 
 				var distance = 200;
 				var speed = 200;
+				var jumpspeed = 300;
 
 				var moveit = function(direction) {
 					moving = true;
@@ -78,37 +79,20 @@ jQuery( document ).ready( function( $ ) {
 							}, speed);
 						}, speed);
 					}, speed);
+				};
 
-					// $('.Bill__1').attr("class", "Bill__1 is-hidden");
-					// $('.Bill__2').attr("class", "Bill__2 is-hidden");
-					// $('.Bill__9').attr("class", "Bill__9 is-hidden");
-					// $('.Bill__3').attr("class", "Bill__3");
-					// setTimeout(function() { 
-					// 	$('.Bill__3').attr("class", "Bill__3 is-hidden");
-					// 	$('.Bill__4').attr("class", "Bill__4");
-					// 	setTimeout(function() { 
-					// 		$('.Bill__4').attr("class", "Bill__4 is-hidden");
-					// 		$('.Bill__5').attr("class", "Bill__5");
-					// 		setTimeout(function() { 
-					// 			$('.Bill__5').attr("class", "Bill__5 is-hidden");
-					// 			$('.Bill__6').attr("class", "Bill__6");
-					// 			setTimeout(function() { 
-					// 				$('.Bill__6').attr("class", "Bill__6 is-hidden");
-					// 				$('.Bill__7').attr("class", "Bill__7");
-					// 				setTimeout(function() { 
-					// 					$('.Bill__7').attr("class", "Bill__7 is-hidden");
-					// 					$('.Bill__8').attr("class", "Bill__8");
-					// 					setTimeout(function() { 
-					// 						$('.Bill__8').attr("class", "Bill__7 is-hidden");
-					// 						$('.Bill__9').attr("class", "Bill__9");
-					// 						Engine.bill.move(); 
-					// 						moving = false;
-					// 					}, speed);
-					// 				}, speed);
-					// 			}, speed);
-					// 		}, speed);
-					// 	}, speed);
-					// }, speed);
+				var jumpit = function(direction) {
+
+
+
+					// unbind the keydown action to prevent repeats
+					$(document).off('keydown');
+
+					$('.Bill').addClass('is-jumping');
+					setTimeout(function() { 
+						$('.Bill').removeClass('is-jumping');
+						Engine.bill.move(); 
+					}, jumpspeed);
 				};
 
 
@@ -129,14 +113,32 @@ jQuery( document ).ready( function( $ ) {
 
 						moveit("left");
 					}
+					else if(keyCode == "83"){ // 'S'
+
+						jumpit();
+					}
 				});
 			},
 
 			money : function() {
-				$('.bling').hover(function() {
+				$('.SiteHeader').hover(function() {
 					$('.Bill').addClass('is-golden');
 				}, function() {
 					$('.Bill').removeClass('is-golden');
+				});
+			},
+
+			explode : function() {
+				$('.Bill svg').click(function() {
+					$('.Bill').toggleClass('is-explode');
+				});
+			},
+
+			intense : function() {
+				$('.Bill svg').hover(function() {
+					$('.Bill').addClass('is-intense');
+				}, function() {
+					$('.Bill').removeClass('is-intense');
 				});
 			}
 		}
@@ -145,6 +147,8 @@ jQuery( document ).ready( function( $ ) {
 	Engine.bill.init();
 	Engine.bill.move();
 	Engine.bill.money();
+	Engine.bill.explode();
+	Engine.bill.intense();
 
 	//setTimeout(function() {
 	//	Engine.bill.breath();
